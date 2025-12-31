4c2a9f8aff772f50ee8633599e13fc7f1a1cc378
Vakiti Manoj <mahimanoj083@gmail.com>
2025-12-31 16:28:14 +0530

    Standardize Malayalam locale code to ml

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -277,7 +277,7 @@ i18n
     resources: {},
     lng: initialLanguage, // Set the language immediately
     fallbackLng: 'en',
-    supportedLngs: ['en', 'te', 'ta', 'es', 'bn', 'hi', 'de', 'af', 'ar', 'as', 'brx', 'zh', 'doi', 'nl', 'fr', 'it', 'pt', 'pl', 'sv', 'el', 'ne', 'pa', 'ma'],
+    supportedLngs: ['en', 'te', 'ta', 'es', 'bn', 'hi', 'de', 'af', 'ar', 'as', 'brx', 'zh', 'doi', 'nl', 'fr', 'it', 'pt', 'pl', 'sv', 'el', 'ne', 'pa', 'ml'],
     debug: false,
     interpolation: {
       escapeValue: false, // React already escapes values
